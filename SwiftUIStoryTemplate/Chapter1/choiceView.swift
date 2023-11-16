f98d07c0230d99c935268dd25654de845935e176
SyedaH <Syeda.hussain13@gmail.com>
2023-11-16 16:52:47 -0500

    Chapter 1 update

---
+++ SwiftUIStoryTemplate/Chapter1/choiceView.swift
@@ -0,0 +1,8 @@
+//
+//  choiceView.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Syeda Hussain on 11/16/23.
+//
+
+import Foundation
---
+++ SwiftUIStoryTemplate/Chapter1/movingDayView.swift
@@ -0,0 +1,8 @@
+//
+//  movingDayView.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Syeda Hussain on 11/16/23.
+//
+
+import Foundation
