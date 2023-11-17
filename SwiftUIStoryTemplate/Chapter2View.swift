02a75f7ddff56f1763c55cabbc806ab9e8dc851d
SyedaH <Syeda.hussain13@gmail.com>
2023-11-17 13:28:47 -0500

    ghaida

--- SwiftUIStoryTemplate/Chapter2View.swift
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct Chapter2View: View {
     var body: some View {
         Text("Chapter 2")
+        
+        
+        
     }
 }
 struct Chapter2View_Previews: PreviewProvider {
